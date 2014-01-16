e96d7fdfb53198b785bd59cfb67a4a387469360a
Grayson Carroll <carrollgt91@gmail.com>
2014-01-15 21:13:27 -0600

    Adds the remainder of the stubs for the Politicians endpoint

--- src/main/java/com/poliana/web/PoliticianController.java
+++ src/main/java/com/poliana/web/PoliticianController.java
@@ -23,66 +23,42 @@ import static com.poliana.core.time.TimeService.CURRENT_CONGRESS;
 /**
  * @author Grayson Carroll
  * @date 1/15/14
- *
- * /politician
-
-/contributions
-/organizations
-?politician_id=XXXXXX *
-?organization_id=XXXXX
-
-/industries
-?politician_id=XXXXXX *
-?industry_id=XXX
-?category_id=XXXXX
-
-/pacs
-?politician_id=XXXXXX *
-?pac_id=XXXX
-
-/expenditures
-
-/committees
-
-/votes
-
-/sponsorships
  */
 
 @Controller
-@RequestMapping("/politician/")
+@RequestMapping("/politicians/")
 public class PoliticianController extends AbstractBaseController {
 
     private static final Logger logger = Logger.getLogger(PoliticianController.class);
 
 
     /**
-     * Grabs politician data for a given bioguide and congresssional cycle.
-     * @param bioguideId
+     * Politician root
      * @param congress
+     * @param fields
      * @return
      */
-    @RequestMapping(value="/{bioguide_id}/", params={"congress, fields"}, method = RequestMethod.GET)
-    public @ResponseBody String getPolitician (
-            @PathVariable("bioguide_id") String bioguideId,
+    @RequestMapping(value="", params={"congress, fields"}, method = RequestMethod.GET)
+    public @ResponseBody String getRoot (
             @RequestParam(value = "congress", required = false, defaultValue = CURRENT_CONGRESS) Integer congress,
             @RequestParam(value = "fields", required = false, defaultValue = "") String fields){
 
 
-        return "politician whose bioguide is " + bioguideId + ", in the congress " + congress + "with fields " + fields;
+        return "politician root, congress " + congress + "with fields " + fields;
     }
 
     /**
      * Grabs politician data for a given bioguide and congresssional cycle.
      * @param bioguideId
      * @param congress
+     * @param fields
      * @return
      */
     @RequestMapping(value="/{bioguide_id}/", params={"congress, fields"}, method = RequestMethod.GET)
     public @ResponseBody String getPolitician (
-           @PathVariable("bioguide_id") String bioguideId,
-           @RequestParam(value = "congress", required = false, defaultValue = CURRENT_CONGRESS) Integer congress,
-           @RequestParam(value = "fields", required = false, defaultValue = "") String fields){
+            @PathVariable("bioguide_id") String bioguideId,
+            @RequestParam(value = "congress", required = false, defaultValue = CURRENT_CONGRESS) Integer congress,
+            @RequestParam(value = "fields", required = false, defaultValue = "") String fields) {
 
 
         return "politician whose bioguide is " + bioguideId + ", in the congress " + congress + "with fields " + fields;
@@ -94,12 +70,44 @@ public class PoliticianController extends AbstractBaseController {
      * @param congress
      * @return
      */
-    @RequestMapping(value="/{bioguide_id}/votes/", params={"congress"}, method = RequestMethod.GET)
-    public @ResponseBody String getPoliticianVotes (
+    @RequestMapping(value="/{bioguide_id}/votes/", params={"congress, fields"}, method = RequestMethod.GET)
+    public @ResponseBody String getVotes (
             @PathVariable("bioguide_id") String bioguideId,
-            @RequestParam(value = "congress", required = false, defaultValue = CURRENT_CONGRESS) Integer congress) {
+            @RequestParam(value = "congress", required = false, defaultValue = CURRENT_CONGRESS) Integer congress,
+            @RequestParam(value = "fields", required = false, defaultValue = "") String fields) {
 
 
         return "votes for politician whose bioguide is " + bioguideId + ", in the congress " + congress;
+    }
+
+    /**
+     * Grabs expenditure data for a politician identified by the given bioguide and congresssional cycle.
+     * @param bioguideId
+     * @param congress
+     * @return
+     */
+    @RequestMapping(value="/{bioguide_id}/expenditures/", params={"congress, fields"}, method = RequestMethod.GET)
+    public @ResponseBody String getExpenditures (
+            @PathVariable("bioguide_id") String bioguideId,
+            @RequestParam(value = "congress", required = false, defaultValue = CURRENT_CONGRESS) Integer congress,
+            @RequestParam(value = "fields", required = false, defaultValue = "") String fields) {
+
+
+        return "expenditures for politician whose bioguide is " + bioguideId + ", in the congress " + congress;
+    }
+    /**
+     * Grabs sponsorship data for a politician identified by the given bioguide and congresssional cycle.
+     * @param bioguideId
+     * @param congress
+     * @return
+     */
+    @RequestMapping(value="/{bioguide_id}/sponsorship/", params={"congress, fields"}, method = RequestMethod.GET)
+    public @ResponseBody String getSponsorship(
+            @PathVariable("bioguide_id") String bioguideId,
+            @RequestParam(value = "congress", required = false, defaultValue = CURRENT_CONGRESS) Integer congress,
+            @RequestParam(value = "fields", required = false, defaultValue = "") String fields) {
+
+
+        return "sponsorship for politician whose bioguide is " + bioguideId + ", in the congress " + congress;
     }
 }

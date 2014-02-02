1893056abda9522864a6f4b48862e7758b5bcadc
Grayson Carroll <carrollgt91@gmail.com>
2014-02-02 17:27:25 -0600

    POL-192 Patches the testing on the open secrets sync to validate the correctness of diffing our bulk contribution data with open secrets'

--- src/test/java/com/poliana/core/common/streaming/OpenSecretsSyncUnitTest.java
+++ src/test/java/com/poliana/core/common/streaming/OpenSecretsSyncUnitTest.java
@@ -6,11 +6,13 @@ import org.junit.Test;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import java.io.File;
 import java.net.URL;
 
 import static org.easymock.EasyMock.createStrictControl;
 import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.expectLastCall;
+
 /**
  * @author graysoncarroll
  * @date 2/1/14
@@ -32,9 +34,8 @@ public class OpenSecretsSyncUnitTest {
         this.jedisMock = this.control.createMock(Jedis.class);
         this.osSync.setJedisPool(jedisPoolMock);
 
-        String path = OpenSecretsSyncUnitTest.class.getResource("").getPath();
-        path += "odata_meta.xml";
-        URL url= (new java.io.File(path)).toURI().toURL();
+        String fixturePath = new java.io.File( "./src/test/java/com/poliana/core/common/streaming/odata_meta.xml" ).getCanonicalPath();
+        URL url = new File(fixturePath).toURI().toURL();
         this.osSync.setUrl(url);
     }
 
@@ -44,14 +45,64 @@ public class OpenSecretsSyncUnitTest {
         expect(this.jedisPoolMock.getResource()).andReturn(jedisMock);
 
         expect(this.jedisMock.exists("CampaignFin14")).andReturn(false);
-        expect(this.jedisMock.exists("CampaignFin12")).andReturn(false);
-
         expect(this.jedisMock.set("CampaignFin14", "1383766050")).andReturn("");
+        this.jedisPoolMock.returnResource(jedisMock);
+        expectLastCall();
+
+        expect(this.jedisPoolMock.getResource()).andReturn(jedisMock);
+
+        expect(this.jedisMock.exists("CampaignFin12")).andReturn(false);
         expect(this.jedisMock.set("CampaignFin12", "1370102184")).andReturn("");
+        this.jedisPoolMock.returnResource(jedisMock);
+        expectLastCall();
 
         this.control.replay();
         this.osSync.sync();
         this.control.verify();
     }
 
+
+    @Test
+    public void testSync__FilesDoesExistNoUpdate() throws Exception {
+        expect(this.jedisPoolMock.getResource()).andReturn(jedisMock);
+
+        expect(this.jedisMock.exists("CampaignFin14")).andReturn(true);
+        expect(this.jedisMock.get("CampaignFin14")).andReturn("1383766050");
+        this.jedisPoolMock.returnResource(jedisMock);
+        expectLastCall();
+
+        expect(this.jedisPoolMock.getResource()).andReturn(jedisMock);
+
+        expect(this.jedisMock.exists("CampaignFin12")).andReturn(true);
+        expect(this.jedisMock.get("CampaignFin12")).andReturn("1370102184");
+        this.jedisPoolMock.returnResource(jedisMock);
+        expectLastCall();
+
+        this.control.replay();
+        this.osSync.sync();
+        this.control.verify();
+    }
+
+    @Test
+    public void testSync__FilesDoesExistWithUpdate() throws Exception {
+        expect(this.jedisPoolMock.getResource()).andReturn(jedisMock);
+
+        expect(this.jedisMock.exists("CampaignFin14")).andReturn(true);
+        expect(this.jedisMock.get("CampaignFin14")).andReturn("1383766000");
+        expect(this.jedisMock.set("CampaignFin14","1383766050")).andReturn("");
+        this.jedisPoolMock.returnResource(jedisMock);
+        expectLastCall();
+
+        expect(this.jedisPoolMock.getResource()).andReturn(jedisMock);
+
+        expect(this.jedisMock.exists("CampaignFin12")).andReturn(true);
+        expect(this.jedisMock.get("CampaignFin12")).andReturn("1370102000");
+        expect(this.jedisMock.set("CampaignFin12", "1370102184")).andReturn("");
+        this.jedisPoolMock.returnResource(jedisMock);
+        expectLastCall();
+
+        this.control.replay();
+        this.osSync.sync();
+        this.control.verify();
+    }
 }

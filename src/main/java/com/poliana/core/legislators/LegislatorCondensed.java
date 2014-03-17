17b7e1e77ee91a0f1f6b6e690e1f656c0d5088a1
Grayson Carroll <carrollgt91@gmail.com>
2014-03-17 16:46:17 -0500

    POL-302 models condensed legislators and terms

--- src/main/java/com/poliana/core/legislators/LegislatorCondensed.java
+++ src/main/java/com/poliana/core/legislators/LegislatorCondensed.java
@@ -1,5 +1,7 @@
 package com.poliana.core.legislators;
 
+import java.util.LinkedList;
+
 /**
  * @author Grayson Carroll
  * @date 3/17/14
@@ -8,6 +10,11 @@ public class LegislatorCondensed {
 
     private String bioguideId;
     private String firstName;
+    private String gender;
+    private String birthday;
+    private String religion;
+    private String party;
+    private LinkedList<Term> terms;
 
     String getBioguideId() {
         return bioguideId;
@@ -24,4 +31,84 @@ public class LegislatorCondensed {
     void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    public String getParty() {
+        return party;
+    }
+
+    public void setParty(String party) {
+        this.party = party;
+    }
+
+    public LinkedList<Term> getTerms() {
+        return terms;
+    }
+
+    public void setTerms(LinkedList<Term> terms) {
+        this.terms = terms;
+    }
+
+    class Term {
+        private String beginTimestamp;
+        private String endTimestamp;
+        private String termType;
+        private LinkedList<Integer> congresses;
+
+        String getBeginTimestamp() {
+            return beginTimestamp;
+        }
+
+        void setBeginTimestamp(String beginTimestamp) {
+            this.beginTimestamp = beginTimestamp;
+        }
+
+        String getEndTimestamp() {
+            return endTimestamp;
+        }
+
+        void setEndTimestamp(String endTimestamp) {
+            this.endTimestamp = endTimestamp;
+        }
+
+        String getTermType() {
+            return termType;
+        }
+
+        void setTermType(String termType) {
+            this.termType = termType;
+        }
+
+        LinkedList<Integer> getCongresses() {
+            return congresses;
+        }
+
+        void setCongresses(LinkedList<Integer> congresses) {
+            this.congresses = congresses;
+        }
+    }
 }
+

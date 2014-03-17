5dcece3aa412e48faf11627bb6c0d9527f1c541c
David Gilmore <david@poliana.com>
2014-03-17 12:57:51 -0500

    Scaffolded necessary classes for the LegislatorCondense service/endpoints POL-302

--- src/main/java/com/poliana/core/legislators/LegislatorCondenseService.java
+++ src/main/java/com/poliana/core/legislators/LegislatorCondenseService.java
@@ -3,7 +3,8 @@ package com.poliana.core.legislators;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * The Legislator Condense service takes the flattened term objects for all legislators and condenses them into
@@ -16,7 +17,7 @@ import java.util.*;
 public class LegislatorCondenseService {
 
     private LegislatorMongoRepo legislatorMongoRepo;
-    private LegislatorRedisRepo legislatorRedisRepo;
+
 
     /**
      * Return all legislators flattened by bioguideId
@@ -24,24 +25,13 @@ public class LegislatorCondenseService {
      */
     public List<LegislatorCondensed> getCondensedLegislators() {
 
-        List<LegislatorCondensed> legislatorsCondensed;
+        legislatorMongoRepo.getAllLegislators();
 
-        if(!legislatorRedisRepo.getCondensedLegislatorsCached()) {
-            // if there aren't any condensed legislators in redis, grab the flat
-            // legislators from mongo, iterate over them, create the condensed legislators,
-            // store them in redis
+        //TODO: Implement logic
+        LegislatorCondensed legislatorCondensed = new LegislatorCondensed();
+        List<LegislatorCondensed> legislatorList = new LinkedList<>();
 
-            Iterator<Legislator> legislatorIterator = legislatorMongoRepo.getAllLegislators();
-            List<LegislatorCondensed> legislatorCondensedList = condenseLegislators(legislatorIterator);
-
-
-            legislatorRedisRepo.setCondensedLegislatorsCached(true);
-        }
-
-        legislatorsCondensed = legislatorRedisRepo.getCondensedLegislators();
-
-        // regardless of how they're created, send 'em out
-        return legislatorsCondensed;
+        return legislatorList;
     }
 
     /**
@@ -59,98 +49,8 @@ public class LegislatorCondenseService {
         return legislatorList;
     }
 
-    /**
-     * Construct a list of LegislatorCondensed objects from a list of flat legislator (term) objects.
-     *
-     * @see LegislatorCondensed
-     * @see Legislator
-     *
-     * @param legislatorIterator
-     * @return
-     */
-    @SuppressWarnings("unchecked")
-    private List<LegislatorCondensed> condenseLegislators(Iterator<Legislator> legislatorIterator) {
-
-        HashMap<String, LegislatorCondensed> legislatorCondensedHashMap = new HashMap<>(15000);
-
-        while (legislatorIterator.hasNext()) {
-
-            Legislator currLegislator = legislatorIterator.next();
-
-
-            String currBioguide = currLegislator.getBioguideId();
-
-            //If a LegislatorCondensed object exists in the map with the current bioguide ID, update it
-            if (legislatorCondensedHashMap.containsKey(currBioguide))
-                createOrUpdateLegislatorCondensed(legislatorCondensedHashMap.get(currBioguide), currLegislator);
-            //Other wise we'll make a new one
-            else {
-                LegislatorCondensed legislatorCondensed = createOrUpdateLegislatorCondensed(new LegislatorCondensed(), currLegislator);
-                legislatorCondensedHashMap.put(currBioguide, legislatorCondensed);
-            }
-
-        }
-
-        return (List) legislatorCondensedHashMap.values();
-    }
-
-    private LegislatorCondensed createOrUpdateLegislatorCondensed(LegislatorCondensed legislatorCondensed, Legislator legislator) {
-
-        legislatorCondensed.setBioguideId( (legislatorCondensed.getBioguideId() == null || legislatorCondensed.getBioguideId().equals("")) ? legislator.getBioguideId() : legislatorCondensed.getBioguideId() );
-        legislatorCondensed.setFirstName( (legislatorCondensed.getFirstName() == null || legislatorCondensed.getFirstName().equals("")) ? legislator.getFirstName() : legislatorCondensed.getFirstName() );
-        legislatorCondensed.setLastName( (legislatorCondensed.getLastName() == null || legislatorCondensed.getLastName().equals("")) ? legislator.getLastName() : legislatorCondensed.getLastName() );
-        legislatorCondensed.setGender( (legislatorCondensed.getGender() == null || legislatorCondensed.getGender().equals("")) ? legislator.getGender() : legislatorCondensed.getGender() );
-        legislatorCondensed.setBirthday( (legislatorCondensed.getBirthday() == null || legislatorCondensed.getBirthday().equals("")) ? legislator.getBirthday() : legislatorCondensed.getBirthday() );
-        legislatorCondensed.setReligion( (legislatorCondensed.getReligion() == null || legislatorCondensed.getReligion().equals("")) ? legislator.getReligion() : legislatorCondensed.getReligion() );
-        legislatorCondensed.setParty( (legislatorCondensed.getParty() == null || legislatorCondensed.getParty().equals("")) ? legislator.getParty() : legislatorCondensed.getParty() );
-
-
-        LegislatorCondensedTerm term = new LegislatorCondensedTerm();
-
-        term.setBeginTimestamp(legislator.getBeginTimestamp());
-        term.setEndTimestamp(legislator.getEndTimestamp());
-        term.setTermType(legislator.getTermType());
-
-        Long currentTimestamp = new java.util.Date().getTime();
-        // Handling incorrect end timestamps; no politician finished their term exactly on unix timestamp zero,
-        // so we adjust there term ends based on the start of the term and the type of term it is.
-        if(legislator.getEndTimestamp() == 0) {
-            //TODO: Verify that this timestamp logic checks for politicians who have been elected within the past 6 years
-            //HACK HACK HACK HACK HACK HACK
-            if(term.getTermType().equals("sen") && (currentTimestamp - term.getBeginTimestamp() >  (31536000*6))) {
-                term.setEndTimestamp(term.getBeginTimestamp() + (31536000*6));
-            }
-
-            else if(term.getTermType().equals("rep") && (currentTimestamp - term.getBeginTimestamp() >  (31536000*2))) {
-                term.setEndTimestamp(term.getBeginTimestamp() + (31536000*2));
-            }
-
-            else {
-                //They've been elected within the past congressional cycle and their timestamp is zero because they are
-                //still in office. We can set a date in the future here or just leave it zero, not sure what to do.
-                //TODO: Figure out what to do with end timestamps for politicians currently in office
-            }
-        }
-
-        // Handling duplicate terms; throw 'em out!
-        boolean keepTerm = true;
-        for (LegislatorCondensedTerm currTerm : legislatorCondensed.getTerms())
-            keepTerm = (term != currTerm);
-
-        if (keepTerm)
-            legislatorCondensed.getTerms().add(term);
-
-
-        return legislatorCondensed;
-    }
-
     @Autowired
     public void setLegislatorMongoRepo(LegislatorMongoRepo legislatorMongoRepo) {
         this.legislatorMongoRepo = legislatorMongoRepo;
-    }
-
-    @Autowired
-    public void setLegislatorRedisRepo(LegislatorRedisRepo legislatorRedisRepo) {
-        this.legislatorRedisRepo = legislatorRedisRepo;
     }
 }
--- src/main/java/com/poliana/core/legislators/LegislatorCondensed.java
+++ src/main/java/com/poliana/core/legislators/LegislatorCondensed.java
@@ -1,24 +1,13 @@
 package com.poliana.core.legislators;
 
-import org.msgpack.annotation.Message;
-
-import java.util.LinkedList;
-
 /**
  * @author Grayson Carroll
  * @date 3/17/14
  */
-@Message
 public class LegislatorCondensed {
 
     private String bioguideId;
     private String firstName;
-    private String lastName;
-    private String gender;
-    private String birthday;
-    private String religion;
-    private String party;
-    private LinkedList<LegislatorCondensedTerm> terms;
 
     String getBioguideId() {
         return bioguideId;
@@ -35,54 +24,4 @@ public class LegislatorCondensed {
     void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(String birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getReligion() {
-        return religion;
-    }
-
-    public void setReligion(String religion) {
-        this.religion = religion;
-    }
-
-    public String getParty() {
-        return party;
-    }
-
-    public void setParty(String party) {
-        this.party = party;
-    }
-
-    public LinkedList<LegislatorCondensedTerm> getTerms() {
-        return terms;
-    }
-
-    public void setTerms(LinkedList<LegislatorCondensedTerm> terms) {
-        this.terms = terms;
-    }
 }
-
-

1bdc5bcdedb9c7f9cfdd638809629167fd47df75
David Gilmore <david@poliana.com>
2014-03-16 22:13:58 -0500

    Error handling working properly. Need to make updates site wide

---
+++ src/main/java/com/poliana/web/error/ResourceNotFoundException.java
@@ -0,0 +1,25 @@
+package com.poliana.web.error;
+
+/**
+ * @author David Gilmore
+ * @date 3/16/14
+ */
+public final class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException() {
+        super();
+    }
+
+    public ResourceNotFoundException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceNotFoundException(final String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(final Throwable cause) {
+        super(cause);
+    }
+
+}
---
+++ src/main/java/com/poliana/web/util/RestPreconditions.java
@@ -0,0 +1,43 @@
+package com.poliana.web.util;
+
+import com.poliana.web.error.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Simple static methods used to validate input. If the Precondition fails, an {@link HttpStatus} code is thrown
+ * @author David Gilmore
+ * @date 3/16/14
+ */
+public final class RestPreconditions {
+
+    private static final String BIOGUIDE_PATTERN = "[A-z]{1,2}\\d{6}";
+
+    private RestPreconditions() {
+        throw new AssertionError();
+    }
+
+    // API
+
+    /**
+     * Check if the bioguideId is valid
+     *
+     * @param bioguideId
+     *            has value true if found, otherwise false
+     * @throws ResourceNotFoundException
+     *             if expression is false, means value not found.
+     */
+    public static void checkValidBioguideId(final String bioguideId) {
+
+        Pattern pattern = Pattern.compile(BIOGUIDE_PATTERN);
+        Matcher matcher = pattern.matcher(bioguideId);
+
+        boolean valid = matcher.matches();
+
+        if (!valid) {
+            throw new ResourceNotFoundException();
+        }
+    }
+}

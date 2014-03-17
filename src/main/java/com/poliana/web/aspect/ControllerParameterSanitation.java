1898698291f39313d1c1b58ba361d27b7ca24ef8
David Gilmore <david@poliana.com>
2014-03-17 13:30:40 -0500

    POL-111. Aspects coming along well. Added bioguide pointcut but have yet to implement its logic

--- src/main/java/com/poliana/web/aspect/ControllerParameterSanitation.java
+++ src/main/java/com/poliana/web/aspect/ControllerParameterSanitation.java
@@ -9,29 +9,28 @@ import org.aspectj.lang.annotation.Pointcut;
 import java.util.Arrays;
 
 /**
- * Will log every invokation of @RequestMapping annotated methods
- * in @Controller annotated beans.
+ * @author David Gilmore
+ * @date 3/17/14
  */
 @Aspect
-public class ControllerLoggingAspect {
+public class ControllerParameterSanitation {
 
     @Pointcut("within(@org.springframework.stereotype.Controller *)")
     public void controller() {}
 
-    @Pointcut("execution(* *(..))")
-    public void methodPointcut() {}
+    @Pointcut("execution(* && args (bioguideId)")
+    public void bioguidePointcut() {}
 
     @Pointcut("within(@org.springframework.web.bind.annotation.RequestMapping *)")
     public void requestMapping() {}
 
-    @Before("controller() && methodPointcut() && requestMapping()")
+    @Before("controller() && bioguidePointcut() && requestMapping()")
     public void aroundControllerMethod(JoinPoint joinPoint) throws Throwable {
         System.out.println("Invoked: " + niceName(joinPoint));
     }
 
-    @AfterReturning("controller() && methodPointcut() && requestMapping()")
+    @AfterReturning("controller() && bioguidePointcut() && requestMapping()")
     public void afterControllerMethod(JoinPoint joinPoint) {
-        System.out.println("Finished: " + niceName(joinPoint));
     }
 
     private String niceName(JoinPoint joinPoint) {

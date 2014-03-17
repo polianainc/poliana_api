3837d8c77f783ad04d2cc342273493e6749ddc1c
David Gilmore <david@poliana.com>
2014-03-17 12:57:51 -0500

    Scaffolded necessary classes for the LegislatorCondense service/endpoints POL-302

---
+++ src/main/java/com/poliana/core/legislators/LegislatorCondensed.java
@@ -0,0 +1,27 @@
+package com.poliana.core.legislators;
+
+/**
+ * @author Grayson Carroll
+ * @date 3/17/14
+ */
+public class LegislatorCondensed {
+
+    private String bioguideId;
+    private String firstName;
+
+    String getBioguideId() {
+        return bioguideId;
+    }
+
+    void setBioguideId(String bioguideId) {
+        this.bioguideId = bioguideId;
+    }
+
+    String getFirstName() {
+        return firstName;
+    }
+
+    void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
--- src/main/java/com/poliana/web/politicians/PoliticianController.java
+++ src/main/java/com/poliana/web/politicians/PoliticianController.java
@@ -1,105 +1,60 @@
 package com.poliana.web.politicians;
 
+import com.poliana.core.legislators.LegislatorCondenseService;
+import com.poliana.core.legislators.LegislatorCondensed;
 import com.poliana.web.common.AbstractBaseController;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import static com.poliana.core.time.TimeService.CURRENT_CONGRESS;
+import java.util.Date;
+import java.util.List;
+
 /**
  * @author Grayson Carroll
  * @date 1/15/14
  */
-
-//@Controller
-@RequestMapping(value = "/politicians/", method = RequestMethod.GET, produces = "application/json", headers = "x-requested-with=XMLHttpRequest")
+@Controller
+@RequestMapping(value = "/politicians", method = RequestMethod.GET)
 public class PoliticianController extends AbstractBaseController {
+
+    private LegislatorCondenseService legislatorCondenseService;
 
     private static final Logger logger = Logger.getLogger(PoliticianController.class);
 
 
     /**
-     * Politician root
-     * @param congress
-     * @param fields
+     * Politician index returns condensed legislator objects
+     *
      * @return
      */
-    @ResponseBody
-    @RequestMapping(value = "", params={"congress, fields"})
-    public String getRoot (
-            @RequestParam(value = "congress", required = false, defaultValue = CURRENT_CONGRESS) Integer congress,
-            @RequestParam(value = "fields", required = false, defaultValue = "") String fields){
+    @RequestMapping(value = "")
+    public @ResponseBody List<LegislatorCondensed> getRoot() {
 
-
-        return "politician root, congress " + congress + "with fields " + fields;
+        return legislatorCondenseService.getCondensedLegislators();
     }
 
     /**
-     * Grabs politician data for a given bioguide and congresssional cycle.
-     * @param bioguideId
-     * @param congress
-     * @param fields
+     * Politician index returns condensed legislator objects in given time range
+     *
      * @return
      */
-    @ResponseBody
-    @RequestMapping(value = "{bioguide_id}", params = {"congress, fields"})
-    public String getPolitician (
-            @PathVariable("bioguide_id") String bioguideId,
-            @RequestParam(value = "congress", required = false, defaultValue = CURRENT_CONGRESS) Integer congress,
-            @RequestParam(value = "fields", required = false, defaultValue = "") String fields) {
+    @RequestMapping(value = "", params={"start, end"})
+    public @ResponseBody List<LegislatorCondensed> getRootByStartAndEnd (
+            @RequestParam(value = "start") @DateTimeFormat(pattern = "MM-dd-yyyy") Date start,
+            @RequestParam(value = "end") @DateTimeFormat(pattern = "MM-dd-yyyy") Date end) {
 
-
-        return "politician whose bioguide is " + bioguideId + ", in the congress " + congress + "with fields " + fields;
+        return legislatorCondenseService.getCondensedLegislators(start.getTime()/1000L, end.getTime()/1000L);
     }
 
-    /**
-     * Grabs vote data for a politician identified by the given bioguide and congresssional cycle.
-     * @param bioguideId
-     * @param congress
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping(value = "{bioguide_id}/votes", params = {"congress, fields"})
-    public String getVotes (
-            @PathVariable("bioguide_id") String bioguideId,
-            @RequestParam(value = "congress", required = false, defaultValue = CURRENT_CONGRESS) Integer congress,
-            @RequestParam(value = "fields", required = false, defaultValue = "") String fields) {
 
-
-        return "votes for politician whose bioguide is " + bioguideId + ", in the congress " + congress;
-    }
-
-    /**
-     * Grabs expenditure data for a politician identified by the given bioguide and congresssional cycle.
-     * @param bioguideId
-     * @param congress
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping(value="{bioguide_id}/expenditures", params = {"congress, fields"})
-    public String getExpenditures (
-            @PathVariable("bioguide_id") String bioguideId,
-            @RequestParam(value = "congress", required = false, defaultValue = CURRENT_CONGRESS) Integer congress,
-            @RequestParam(value = "fields", required = false, defaultValue = "") String fields) {
-
-
-        return "expenditures for politician whose bioguide is " + bioguideId + ", in the congress " + congress;
-    }
-
-    /**
-     * Grabs sponsorship data for a politician identified by the given bioguide and congresssional cycle.
-     * @param bioguideId
-     * @param congress
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping(value="{bioguide_id}/sponsorship", params = {"congress, fields"})
-    public String getSponsorship(
-            @PathVariable("bioguide_id") String bioguideId,
-            @RequestParam(value = "congress", required = false, defaultValue = CURRENT_CONGRESS) Integer congress,
-            @RequestParam(value = "fields", required = false, defaultValue = "") String fields) {
-
-
-        return "sponsorship for politician whose bioguide is " + bioguideId + ", in the congress " + congress;
+    @Autowired
+    public void setLegislatorCondenseService(LegislatorCondenseService legislatorCondenseService) {
+        this.legislatorCondenseService = legislatorCondenseService;
     }
 }
